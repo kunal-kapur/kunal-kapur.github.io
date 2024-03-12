416f2d6e64b144d922a45504fef970ba9a38c421
Kunal Kapur <kunalkapur@Kunals-MacBook-Air.local>
2024-03-11 23:04:50 -0700

    scrolling

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function redirectToPage(url) {
     window.location.href = url;
 }
+
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    const offset = -10
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+}
